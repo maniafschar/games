12df5fdb38b0bb03aa3f0c6ddae764a2f7f76492
Mani Afschar Yazdi <mani.afschar@jq-consulting.de>
2026-01-20 10:39:36 +0100

    demo data

--- src/main/java/com/jq/games/service/AdminService.java
+++ src/main/java/com/jq/games/service/AdminService.java
@@ -7,6 +7,7 @@ import java.time.Duration;
 import java.time.Instant;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.time.ZoneId;
 import java.time.ZoneOffset;
 import java.util.Arrays;
 import java.util.Date;
@@ -191,7 +192,8 @@ public class AdminService {
 	private void createEvent(final LocalDateTime date, final Contact contact, final Location location,
 			final List<String> contacts, final StringBuffer result) {
 		if (this.repository
-				.list("from Event where date=cast('" + date + "' as LocalDate) and contact.id=" + contact.getId()
+				.list("from Event where date=cast('" + date.atZone(ZoneId.of("UTC"))
+						+ "' as LocalDateTime) and contact.id=" + contact.getId()
 						+ " and location.id=" + location.getId(), Event.class)
 				.size() > 0)
 			return;

c7758b2d18b00cce942f3c24f577e80789f6dd34
Mani Afschar Yazdi <mani.afschar@jq-consulting.de>
2026-02-02 19:23:05 +0100

    input date holidays

--- src/web/js/element/InputDate.js
+++ src/web/js/element/InputDate.js
@@ -58,6 +58,9 @@ cell.outdated {
 	opacity: 0.4;
 	cursor: default;
 }
+cell.holiday {
+	color: rgba(255, 140, 80, 0.8);
+}
 cell.selected {
 	background-color: rgba(255, 255, 255, 0.6);
 }
@@ -154,6 +157,7 @@ next::after {
 		var outdated, selectable = this.getAttribute('selectable');
 		var maxMonth = parseInt(year) == max.getFullYear() && parseInt(month) == max.getMonth() + 1;
 		var minMonth = parseInt(year) == min.getFullYear() && parseInt(month) == min.getMonth() + 1;
+		var holidays = this.bankholidays(parseInt(year));
 		for (var i = 1; i <= maxDays; i++) {
 			outdated = maxMonth ? i > max.getDate() : minMonth ? i < min.getDate() : false;
 			if (!outdated && selectable)
@@ -163,6 +167,8 @@ next::after {
 				c += ' weekend';
 			if (i == day)
 				c += ' selected';
+			if (holidays[i + '.' + parseInt(month)])
+				c += ' holiday';
 			s += `<cell ${outdated ? '' : `onclick="this.getRootNode().host.selectDay(${i},true)"`}${c ? ' class="' + c.trim() + '"' : ''}>${i}</cell>`;
 			if ((i + offset) % 7 == 0)
 				s += '<br/>';
@@ -398,5 +404,47 @@ next::after {
 	min() {
 		var min = this.getAttribute('min');
 		return min ? new Date(min) : new Date();
+	}
+	bankholidays(year) {
+		var a = year % 19;
+		var d = (19 * a + 24) % 30;
+		var day = d + (2 * (year % 4) + 4 * (year % 7) + 6 * d + 5) % 7;
+		if (day == 35 || (day == 34 && d == 28 && a > 10))
+			day -= 7;
+
+		var easter = new Date(year, 2, 22);
+		// Die Zahl 86400000 nicht ausklammern, sonst gibt's Probleme bei der Typumwandlung !!
+		easter.setTime(easter.getTime() + 86400000 * day);
+		var convert = function (datla) {
+			var date = new Date(easter.getTime());
+			date.setDate(easter.getDate() + datla);
+			return date.getDate() + '.' + (date.getMonth() + 1);
+		};
+
+		var holidays = {};
+		holidays['1.1'] = true; // Neujahr
+		holidays['6.1'] = true; // Hl. 3 Koenige
+		holidays['1.5'] = true; // Maifeiertag - Tag der Arbeit
+		holidays['15.8'] = true; // Maria Himmelfahrt
+		holidays['3.10'] = true; // Tag der Deutschen Einheit
+		holidays['31.10'] = true; // Reformationstag
+		holidays['1.11'] = true; // Allerheiligen
+		holidays['24.12'] = true; // Weihnachten
+		holidays['25.12'] = true; // 1. Weihnachtstag
+		holidays['26.12'] = true; // 2. Weihnachtstag
+		holidays['31.12'] = true; // Silvester
+		holidays[convert(-52)] = true; // Weiberfastnacht
+		holidays[convert(-48)] = true; // Rosenmontag 
+		holidays[convert(-47)] = true; // Fastnachtsdienstag 
+		holidays[convert(-46)] = true; // Aschermittwoch 
+		holidays[convert(-3)] = true; // Gründonnerstag 
+		holidays[convert(-2)] = true; // Karfreitag 
+		holidays[convert(0)] = true; // Ostersonntag
+		holidays[convert(1)] = true; // Ostermontag 
+		holidays[convert(39)] = true; // Christi Himmelfahrt 
+		holidays[convert(49)] = true; // Pfingstsonntag 
+		holidays[convert(50)] = true; // Pfingstmontag  
+		holidays[convert(60)] = true; // Fronleichnam 
+		return holidays;
 	}
 }

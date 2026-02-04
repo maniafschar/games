79e9adf3c8b0900f54944c9201d4ea8af5749ee9
Mani Afschar Yazdi <mani.afschar@jq-consulting.de>
2026-02-04 11:53:57 +0100

    error reporting

--- src/web/js/dialog.js
+++ src/web/js/dialog.js
@@ -421,7 +421,7 @@ value.participants total {
 					addImage(event.eventImages[i].id, 'med/' + event.eventImages[i].image);
 			}
 			popup.appendChild(document.createElement('label')).innerText = 'Ersteller';
-			popup.appendChild(document.createElement('value')).innerHTML = event.contact.name;
+			popup.appendChild(document.createElement('value')).innerText = event.contact.name;
 			if (api.contactId == event.contact.id) {
 				var button = popup.appendChild(document.createElement('button'));
 				button.innerHTML = '<svg width="128" height="128" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" clip-rule="evenodd" d="M19.424 4.575a2.5 2.5 0 0 0-3.535 0l-1.06 1.061 3.535 3.536-.354.353-.353.354-3.536-3.536-8.839 8.839a.5.5 0 0 0-.136.255l-.708 3.536a.5.5 0 0 0 .589.588l3.535-.707a.5.5 0 0 0 .256-.137L19.424 8.111a2.5 2.5 0 0 0 0-3.536Z" fill="#000000"></path></svg>';

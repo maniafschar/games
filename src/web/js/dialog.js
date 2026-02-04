788378e1cd782506b1a66804d99ca006b2976c4d
Mani Afschar Yazdi <mani.afschar@jq-consulting.de>
2026-02-04 01:55:38 +0100

    dialog.js aktualisieren

--- src/web/js/dialog.js
+++ src/web/js/dialog.js
@@ -457,4 +457,19 @@ value.participants total {
 			});
 		});
 	}
+	static client() {
+		var popup = document.createElement('div');
+		var selection = popup.appendChild(document.createElement('input-selection'));
+		selection.setAttribute('value', api.clientId);
+		var keys = Object.keys(api.clients);
+		for (var i = 0; i < keys.length; i++)
+			selection.add(keys[i], api.clients[keys[i]].name);
+		document.dispatchEvent(new CustomEvent('popup', { detail: { body: popup } }));
+		document.querySelector('dialog-popup').content().querySelector('input-selection').addEventListener('changed', () => {
+			api.clientId = document.querySelector('dialog-popup').content().querySelector('input-selection').getAttribute('value');
+			document.dispatchEvent(new CustomEvent('event'));
+			document.dispatchEvent(new CustomEvent('contact'));
+			document.dispatchEvent(new CustomEvent('popup'));
+		});
+	}
 }

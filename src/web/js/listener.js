18f3975fd362fdbb5e04a4e510299c7871c1789d
Mani Afschar Yazdi <mani.afschar@jq-consulting.de>
2026-03-09 12:11:19 +0100

    event table max height

--- src/web/js/listener.js
+++ src/web/js/listener.js
@@ -59,7 +59,7 @@ class listener {
 
 			var table = document.querySelector('event sortable-table');
 			table.list = events;
-			table.style('tr.past{opacity:0.4;}');
+			table.style('tr.past{opacity:0.4;}tbody{max-height:18em;}');
 			if (!table.columns.length) {
 				var now = new Date();
 				table.setOpenDetail(dialog.event);

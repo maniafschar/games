f451d96d6de12b85f99e2d3dd7ebb195a121f527
Mani Afschar Yazdi <mani.afschar@jq-consulting.de>
2026-03-22 06:36:28 +0100

    cosmetics

--- src/web/js/listener.js
+++ src/web/js/listener.js
@@ -170,7 +170,7 @@ class listener {
 						document.querySelector('dialog-popup').content().querySelectorAll('value.participants input').forEach(
 							input => {
 								var x = input.value?.replace(',', '.');
-								if (!x || isNaN(x))
+								if (x?.trim() == '' || isNaN(x))
 									return;
 								x = parseFloat(x);
 								var item = document.querySelector('dialog-popup').content().querySelector('value.participants item[i="' + ui.parents(input, 'participant').getAttribute('i') + '"]');

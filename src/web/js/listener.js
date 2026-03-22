956be02239e11c14b777edd15c61e1d24587c6ae
Mani Afschar Yazdi <mani.afschar@jq-consulting.de>
2026-03-22 11:42:37 +0100

    count events

--- src/web/js/listener.js
+++ src/web/js/listener.js
@@ -130,6 +130,7 @@ class listener {
 				}
 			}
 			calendar.render();
+			document.querySelector('count').innerText = document.querySelector('sortable-table')._root.querySelectorAll('tr.past') + ' / ' + events.length;
 			document.querySelector('history').scrollLeft = document.querySelector('history').scrollWidth;
 			document.querySelector('event').style.display = '';
 			document.querySelector('event').previousElementSibling.style.display = 'block';
